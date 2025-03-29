30f67144221583e7bbab8917c4a34d2664dd4443
Saba Ahmad <sabaahmad.sp@gmail.com>
2025-03-29 19:40:21 +0530

    Adds firebase and initialized authentication for FireChat

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,5 +1,29 @@
-import Add from "../assets/Add.png"
+import Add from "../assets/Add.png";
+import { createUserWithEmailAndPassword } from "firebase/auth"
+import { auth } from "../firebase";
+
 const Register = () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const displayName = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+    const file = e.target[3].files[0];
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed up
+        const user = userCredential.user;
+        console.log(user);
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+      });
+  };
+
   return (
     <>
       <div className="h-dvh bg-[#9ACBD0] flex flex-col items-center justify-center">
@@ -7,8 +31,10 @@ const Register = () => {
           <span className="font-semibold text-2xl tracking-wider font-mono text-[#006A71]">
             FireChat
           </span>
-          <span className="font-medium tracking-wide text-[#48A6A7] mb-2">Register</span>
-          <form className="flex flex-col gap-3.5">
+          <span className="font-medium tracking-wide text-[#48A6A7] mb-2">
+            Register
+          </span>
+          <form onSubmit={handleSubmit} className="flex flex-col gap-3.5">
             <input
               className="py-1 px-2 border-b-1  text-sm focus:outline-none  focus:border-b-2 focus:border-b-[#9ACBD0]
              "
@@ -28,8 +54,13 @@ const Register = () => {
               placeholder="Password"
             />
             <input className="hidden" type="file" id="file" />
-            <label className="flex items-center gap-4 px-0.5 mb-1 text-xs text-[#48A6A7]" htmlFor="file">
-              <img src={Add} className="w-7" alt="add-image-logo" />Add Profile Picture</label>
+            <label
+              className="flex items-center gap-4 px-0.5 mb-1 text-xs text-[#48A6A7]"
+              htmlFor="file"
+            >
+              <img src={Add} className="w-7" alt="add-image-logo" />
+              Add Profile Picture
+            </label>
             <button
               className="bg-blue-400 rounded-2xl hover:bg-blue-500 py-1 text-white font-semibold tracking-wider"
               type="submit"
---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAYWyFh66ZnCCMYMuLSm8cP1q7gqCmaaXk",
+  authDomain: "firechat-f7823.firebaseapp.com",
+  projectId: "firechat-f7823",
+  storageBucket: "firechat-f7823.firebasestorage.app",
+  messagingSenderId: "912956390190",
+  appId: "1:912956390190:web:4ed0d876aa38cfd2f0d8bf",
+  measurementId: "G-8CCL17FY8J"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
+const analytics = getAnalytics(app);
